da2ba88d3c68518c90f2ed204df4443c07f4824b
Roman Smolin <romasmolin29@gmail.com>
2023-12-14 21:02:21 +0200

    Refactor

--- client/src/js/modules/formData.js
+++ client/src/js/modules/formData.js
@@ -2,32 +2,8 @@ const formData = {};
 
 // TODO: Refator code
 
-function updatePropertyType(propertyType) {
-    formData["propertyType"] = propertyType;
-}
-
-function updateZipCode(inputId, zipCode) {
-    formData[inputId] = zipCode
-}
-
-function updatePropertySize(size) {
-    formData["movesize"] = size;
-}
-
-function updateMovingDate(movingDate) {
-    formData["movedte"] = movingDate;
-}
-
-function updateName(inputId, name) {
-    formData[inputId] = name;
-}
-
-function updateEmail(inputId, email) {
-    formData[inputId] = email;
-}
-
-function updatePhone(inputId, phone) {
-    formData[inputId] = phone;
+function updateFormData(key, value) {
+    formData[key] = value;
 }
 
 function updateCountry(country, type) {
